0fce140ff8cac6b4771108a1e39af67007eaa1ab
Luis Abarca <luisabarca@gmail.com>
2022-09-01 00:08:04 -0500

    componente para menú superior con stitches

---
+++ react/src/components/menu-superior.tsx
@@ -0,0 +1,47 @@
+import { styled } from "@stitches/react";
+
+interface IMenuSuperiorProps {
+  elementos: string[];
+  titulo?: string;
+}
+
+// Copiado de los estilos css
+const MenuWrapper = styled("div", {
+  backgroundColor: "#282c34",
+  minHeight: "10vh",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white",
+  margin: "0",
+  padding: "0",
+});
+
+const Menu = styled("ul", {
+  margin: "0",
+  padding: "0",
+  display: "flex"
+});
+
+const MenuOption = styled("li", {
+  listStyle: "none",
+  margin: "0",
+  padding: "4px 12px",
+})
+
+export const MenuSuperior: React.FC<IMenuSuperiorProps> = ({ titulo, elementos }) => {
+  return (
+    <MenuWrapper>
+      <Menu>
+        <MenuOption>{titulo ?? ""}</MenuOption>
+
+        {elementos.map((element) => (
+          <MenuOption>{element}</MenuOption>
+        ))}
+
+      </Menu>
+    </MenuWrapper>
+  );
+};
