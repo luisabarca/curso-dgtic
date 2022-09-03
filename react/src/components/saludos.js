ccaef4042f72ce7b05a088fd5a1a41e8d50561fc
Luis Abarca <luisabarca@gmail.com>
2022-09-02 23:24:50 -0500

    agrega render condicional

--- react/src/components/saludos.js
+++ react/src/components/saludos.js
@@ -1,9 +1,25 @@
 // stateless, presentación
+
+import { Button } from "./button";
+
 // destructuring the props
-export const Saludo = ({ cargo, nombre }) => {
+export const Saludo = (props) => {
+  if (!props.cargo && !props.nombre) return null;
+
+  if (!props.nombre) return <h1>Hola Desconocido</h1>;
+
+  if (props.nombre === "mundo") return <h1>Que original!</h1>;
+
+  // if (props.isLoading) return <LoadingComponent />
+
   return (
     <h3>
-      Hola {cargo} {nombre}
+      {props.mostrarHola ? <span>Hola </span> : null}
+      {props.cargo ?? ""} {props.nombre ?? ""}
+      
+      {
+        props.isAdmin ? <Button>Eliminar</Button> : null
+      }
     </h3>
   );
 };
