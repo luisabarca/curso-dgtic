82d69b5c3e4ada8d74a487a6f11467f89578708e
Luis Abarca <luisabarca@gmail.com>
2022-09-02 23:24:16 -0500

    cambia setState por funcional y agrega useEffect para mostrar ganador

--- react/src/components/contador.jsx
+++ react/src/components/contador.jsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./button";
 
 export const Contador = ({ valorInicial }) => {
   const [valor, setValor] = useState(valorInicial ?? 0);
+  const [esGanador, setEsGanador] = useState(false);
 
   const handleClick = () => {
-    setValor(valor + 1);
+    setValor((valorActual) => valorActual + 1);
   };
+
+  // Se ejecuta en cada renderizado
+  useEffect(() => {
+    console.log("desde useEffect");
+  });
+
+  useEffect(() => {
+    // Cargamos datos del servidor
+    // if (datos.length < 1) {
+    //   fetch(`${process.env.REACT_APP_API_ENDPOINT}/usuarios`).then((data) => {
+    //     setDatos(data);
+    //   });
+
+    //   obtenerUsuarios();
+    // }
+  }, []);
+
+  useEffect(() => {
+    if (valor >= process.env.REACT_APP_NUMERO_GANADOR) {
+      setEsGanador(true);
+    }
+  }, [valor]);
 
   return (
     <div>
+      {
+        esGanador && <h1>Ganaste!!</h1>
+      }
+
       <div>Contador: {valor}</div>
 
       <Button onClick={handleClick}>Actualizar</Button>
