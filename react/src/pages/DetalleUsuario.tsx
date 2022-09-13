32ab641379fdae8235b7083f4e4474dcef186ded
Luis Abarca <luisabarca@gmail.com>
2022-09-12 20:44:45 -0500

    agrega página de detalle de un usuario

---
+++ react/src/pages/DetalleUsuario.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Usuario } from "../types";
+
+export const DetalleUsuarioPage = () => {
+  const params = useParams();
+
+  const [usuario, setUsuario] = useState<Usuario | null>(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${params.usuarioId}`)
+      .then(async (response) => {
+        const datos = await response.json();
+
+        setUsuario(datos);
+      })
+      .catch((error) => {
+        console.log(error);
+        throw new Error("asasasa");
+      });
+  }, [params]);
+
+  if (!usuario) return <h1>Loading...</h1>
+
+  return (
+    <div>
+        <h1>Detalle de Usuario</h1>
+        <p>{usuario.name}</p>
+    </div>
+  );
+};
