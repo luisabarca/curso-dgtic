8cf6bac8de4a21ddf8c3a0b828c841f2b2072a97
Luis Abarca <luisabarca@gmail.com>
2022-09-14 23:10:21 -0500

    mueve la sección GraphQL a una nueva página

---
+++ react/src/pages/graphql/index.tsx
@@ -0,0 +1,37 @@
+import { useQueryObtenerEmpresas } from "./querys";
+
+const CargandoDatos = ({ texto }: { texto?: string }) => <p>Cargando {texto}</p>;
+const ErrorComponente = () => <p>Ocurrió un error</p>;
+
+export const GraphqlPage = () => {
+  
+  const [result] = useQueryObtenerEmpresas();
+  const { data, error, fetching } = result;
+
+  return (
+    <>
+
+      {
+        fetching && (
+          <CargandoDatos texto="compañias" />
+        )
+      }
+
+      {
+        error && (
+          <ErrorComponente />
+        )
+      }
+
+      {
+        !fetching && data.allCompanies.length > 0 && data.allCompanies.map((item: any) => {
+          return (
+            <li>{item.name}</li>
+          )
+        })
+      }
+    </>
+  );
+};
+
+export default GraphqlPage;
