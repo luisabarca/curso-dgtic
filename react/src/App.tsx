91987d8222e5e3895f5deca4ed83c8d91cbd878d
Luis Abarca <luisabarca@gmail.com>
2022-09-14 22:44:23 -0500

    agrega división de código para producción

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,47 +1,61 @@
+// Default exports
+import React, { Suspense } from "react";
 import { styled } from "@stitches/react";
-import { MenuSuperior } from "./components/menu-superior";
-import { TipoUsuario } from "./types";
-import { guardarUsuario } from "./utils/guardarUsuario";
 import { Route, Routes } from "react-router-dom";
-import { InicioPage } from "./pages/Inicio";
-import { NoticiasPage } from "./pages/Noticias";
-import { UsuariosPage } from "./pages/Usuarios";
-import { DetalleNoticiaPage } from "./pages/DetalleNoticia";
-import { DetalleUsuarioPage } from "./pages/DetalleUsuario";
-import { AppContextProvider } from "./context/app-context";
 import { createClient, Provider } from "urql";
+import { AppContextProvider } from "./context/app-context";
+
+// Named exports
+const MenuSuperior = React.lazy(() => import("./components/menu-superior"));
+const InicioPage = React.lazy(() => import("./pages/Inicio"));
+const NoticiasPage = React.lazy(() => import("./pages/Noticias"));
+const UsuariosPage = React.lazy(() => import("./pages/Usuarios"));
+const DetalleNoticiaPage = React.lazy(() => import("./pages/DetalleNoticia"));
+const DetalleUsuarioPage = React.lazy(() => import("./pages/DetalleUsuario"));
 
 const AppWrapperStyled = styled("div", {
   textAlign: "center",
 });
 
+const SkeletonMenu = styled("div", {
+  backgroundColor: "#333",
+  height: 90,
+  width: "100%",
+});
+
+// La URL podría venir en una variable de ambiente 
+// REACT_APP_API_GRAPHQL_ENDPOINT
 const client = createClient({
   url: "http://localhost:9002/graphql",
 });
 
 function App() {
   // Obtiene la config en JSON y lo transforma en objeto
-  const config = JSON.parse(process.env.REACT_APP_CONFIG_JSON ?? "");
-  console.log("config", config);
+  // const config = JSON.parse(process.env.REACT_APP_CONFIG_JSON ?? "");
 
   return (
     <AppWrapperStyled>
       <Provider value={client}>
         <AppContextProvider>
-          <MenuSuperior />
-          <Routes>
-            <Route path="/" element={<InicioPage />} />
-            <Route path="/noticias" element={<NoticiasPage />} />
-            <Route
-              path="/noticias/:noticiaId"
-              element={<DetalleNoticiaPage />}
-            />
-            <Route
-              path="/usuarios/:usuarioId"
-              element={<DetalleUsuarioPage />}
-            />
-            <Route path="/usuarios" element={<UsuariosPage />} />
-          </Routes>
+          <Suspense fallback={<SkeletonMenu />}>
+            <MenuSuperior />
+          </Suspense>
+
+          <Suspense fallback={<div>Cargando...</div>}>
+            <Routes>
+              <Route path="/" element={<InicioPage />} />
+              <Route path="/noticias" element={<NoticiasPage />} />
+              <Route
+                path="/noticias/:noticiaId"
+                element={<DetalleNoticiaPage />}
+              />
+              <Route
+                path="/usuarios/:usuarioId"
+                element={<DetalleUsuarioPage />}
+              />
+              <Route path="/usuarios" element={<UsuariosPage />} />
+            </Routes>
+          </Suspense>
         </AppContextProvider>
       </Provider>
     </AppWrapperStyled>
--- react/src/pages/DetalleNoticia.tsx
+++ react/src/pages/DetalleNoticia.tsx
@@ -1,10 +1,9 @@
 import { useParams } from "react-router-dom";
 
-export const DetalleNoticiaPage = () => {
+export default function DetalleNoticiaPage() {
     const params = useParams();
-    console.log(params);
-
+    
     return (
-        <h1>Detalle de Noticias</h1>
+        <h1>Detalle de Noticia: {params.noticiaId}</h1>
     );
 }
