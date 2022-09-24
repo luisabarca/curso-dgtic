5ddadab15290e0b9e7e683ced8666506dfb8c36d
Luis Abarca <luisabarca@gmail.com>
2022-09-23 22:40:53 -0500

    agrega lista de noticias con SSR

--- nextjs/pages/noticias/index.tsx
+++ nextjs/pages/noticias/index.tsx
@@ -1,5 +1,49 @@
-export default function NoticiasPage() {
-    return (
-        <div>Noticias</div>
-    )
+import Link from "next/link";
+import { useState } from "react";
+import { getNoticias } from "../../services/noticias";
+
+type NoticiasPageProps = {
+  // La magia de Typescript para saber el tipo de dato que retorna la función
+  // dame el tipo de datos (typeof) que retorna (ReturnType) getNoticias()
+  // pero espera (Awaited) porque es función que retorna una promise
+  noticias: Awaited<ReturnType<typeof getNoticias>>;
+}
+
+// Cliente
+export default function NoticiasPage({ noticias }: NoticiasPageProps) {
+  const [noticiasList, setNoticiasList] = useState(noticias);
+  
+  const actualizar = async () => {
+    const noticias = await getNoticias();
+    setNoticiasList(noticias);
+  }
+
+  return (
+    <>
+      <h1>Noticias</h1>
+      {noticiasList && noticiasList.length > 0 &&
+        noticiasList.map((item: any) => {
+          return (
+            <div key={`${item.id}`}>
+              <Link href={`/noticias/${item.id}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
+          );
+        })}
+
+        <button onClick={actualizar}>Actualizar</button>
+    </>
+  );
+}
+
+// Servidor (SSR)
+export async function getServerSideProps() {
+  const noticias = await getNoticias();
+
+  return {
+    props: {
+      noticias: noticias.slice(0, 5),
+    },
+  };
 }
