6d24c92d59fd7a6d8c66e2fee956305c42ec6f15
Luis Abarca <luisabarca@gmail.com>
2022-10-24 23:27:29 -0500

    mueve CRUD con prisma

--- node/index.ts
+++ node/index.ts
@@ -1,79 +1,25 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { createUserRoutes } from "./routes/usuarios";
 
-async function createUser() {
-    const user = await prisma.user.create({
-        data: {
-            name: "Vicente Guerrero",
-            email: "vicente@guerrero.gob.mx",
-        }
+const app = express();
+
+const options = {
+    origin: "http://localhost:3002",
+};
+
+app.use(cors(options));
+app.use(bodyParser.json());
+
+createUserRoutes(app);
+
+app.get("/", (req, res) => {
+    res.send({
+        status: "OK",
     });
-}
+});
 
-async function createUserAndContent() {
-    const user = await prisma.user.create({
-        data: {
-            name: "Nicolas Bravo",
-            email: "nicolas@guerrero.gob.mx",
-            posts: {
-                create: {
-                    title: "Noticias de Nicolas",
-                    content: "Contenido de noticia"
-                },
-            }
-        }
-    });
-
-    console.log(user);
-}
-
-async function createPost() {
-    await prisma.post.createMany({
-        data: [
-            {
-                title: "Noticia 1",
-                authorId: 1,
-            },
-            {
-                title: "Noticia 2",
-                authorId: 1,
-            },
-        ]
-    })
-}
-
-async function getUsuarios() {
-    const usuarios = await prisma.user.findMany({
-        include: {
-            posts: true,
-        },
-    });
-
-    console.log(usuarios);
-    console.log(usuarios[0].posts);
-}
-
-async function update() {
-    await prisma.user.update({
-        data: {
-            email: "luis@bplux.mx",
-        },
-        where: {
-            id: 1,
-        }
-    });
-}
-
-async function deleteUser() {
-    await prisma.user.delete({
-        where: {
-            id: 4,
-        }
-    });
-}
-
-createUserAndContent();
-getUsuarios();
-// createPost();
-// update();
-// deleteUser();
+app.listen(3001, () => {
+    console.log("Servidor activo")
+});
